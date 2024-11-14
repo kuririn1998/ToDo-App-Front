a6f59a4067d2050f9665116a7ad8ad2945fb5439
kuririn1998 <rinrin.kuribayashi@gmail.com>
2024-11-14 18:37:54 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/todo")
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
